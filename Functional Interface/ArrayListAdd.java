a264312ede41992b68bb5298cf1fcc80c0cbb8d7
malleshgithub <malleshams.coign@gmail.com>
2025-01-27 10:32:00 +0530

    Add files via upload
    ArrayList values with Ascending order using comparator.

---
+++ Functional Interface/ArrayListAdd.java
@@ -0,0 +1,35 @@
+/*Ascending order using ArrayList*/
+package FunctionalInterface;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+class MyComparator implements Comparator<Integer>{
+	public int compare(Integer I1, Integer I2) {
+		if(I1<I2) {
+			return -1;
+		}else if(I1>I2) {
+			return 1;
+		}else {
+			return 0;
+		}
+	}
+} 
+public class ArrayListAdd {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> l=new ArrayList<Integer>();
+		l.add(20);
+		l.add(10);
+		l.add(25);
+		l.add(5);
+		l.add(30);
+		l.add(0);
+		l.add(15);
+		System.out.println(l);
+		Collections.sort(l, new MyComparator());
+		System.out.println(l);
+	}
+
+}
